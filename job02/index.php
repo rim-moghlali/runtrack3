df11f21d1982a75359e48cd4984eade14fcffead
rim-moghlali <rim-moghlali@laplateforme.io>
2023-01-24 20:11:12 +0100

    javascript

---
+++ job02/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+
+        <script>
+            var str = "Hello Javascript!";
+            alert(str);
+        </script>
+    </head>
+    <body>
+
+    </body>
+</html>
