2322be769abb3f1012e71e23d704a22e68eae09d
rim-moghlali <rim-moghlali@laplateforme.io>
2023-02-24 14:30:14 +0100

    JQuery - jour03

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -11,6 +11,7 @@
     <script scr="script.js">
         </script>
 </head>
+
 <body>
     <p id="text" style = "display: none" >
     Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,
