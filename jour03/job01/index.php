3eb138135861264bb99cace9aa240b5d85de8f41
rim-moghlali <rim-moghlali@laplateforme.io>
2023-02-24 14:27:59 +0100

    jour03

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -24,15 +24,14 @@
 <script>
 
    function show(){
-    
     const btnshow = document.querySelector('#show');
     btnshow = document.getElementById("text").style.display ='block';
-}
+    }
 
-function hide(){
+    function hide(){
     const btnhide = document.querySelector('#hide');
     btnhide = document.getElementById("text").style.display = 'none';
-} 
+    } 
 
 </script>
     
